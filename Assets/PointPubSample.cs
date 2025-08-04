e24b2b6c9e12e657edb10f87fc54e0ef9030fbf9
ggone212 <155419769+ggone212@users.noreply.github.com>
2025-08-04 11:00:28 +0900

    [chore] 샘플 프로젝트 수정

--- Assets/PointPubSample.cs
+++ Assets/PointPubSample.cs
@@ -1,20 +1,24 @@
-using System.Collections;
 using System.Collections.Generic;
 using PointPubUnityPlugin;
 using UnityEngine;
 
 public class PointPubSample : MonoBehaviour {
-    private AndroidJavaObject pluginInstance;
 
-    void Start() {
+    private Vector2 scrollPosition;
+    private List<string> buttonTitles = new List<string> {
+        "EnableLogTrace",
+        "StartOfferwall",
+    };
 
+    void Start()
+    {
+        Debug.Log("The application started.");
     }
 
-    void Update() {
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            if (Input.GetKeyUp(KeyCode.Escape))
-            {
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android) {
+            if (Input.GetKeyUp(KeyCode.Escape)) {
                 Application.Quit();
             }
         }
@@ -22,20 +26,65 @@ public class PointPubSample : MonoBehaviour {
 
     void OnGUI()
     {
-        var fontSize = (int)(0.035f * Screen.width);
-        var buttonWidth = 0.35f * Screen.width;
-        var buttonHeight = 0.15f * Screen.height;
+        // Safe area 계산
+        Rect safeArea = Screen.safeArea;
+
+        // UI 크기 비율 설정
+        int fontSize = (int)(Screen.width * 0.035f);
+        float buttonWidth = safeArea.width * 0.7f;
+        float buttonHeight = safeArea.height * 0.1f;
+        float spacing = Screen.height * 0.02f;
 
         GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
         buttonStyle.fontSize = fontSize;
 
-        float buttonX = (Screen.width - buttonWidth) / 2;
-        float buttonY = (Screen.height - buttonHeight) / 2;
+        // 스크롤 뷰의 전체 높이에 상단 여백 추가
+        float totalContentHeight = buttonTitles.Count * (buttonHeight + spacing) + spacing;
+        Rect viewRect = new Rect(0, 0, safeArea.width, totalContentHeight);
 
-        if (GUI.Button(new Rect(buttonX, buttonY, buttonWidth, buttonHeight), "Start Offerwall", buttonStyle))
+        scrollPosition = GUI.BeginScrollView(
+            new Rect(safeArea.x, safeArea.y, safeArea.width, safeArea.height),
+            scrollPosition,
+            viewRect
+        );
+
+        // 최상단 여백
+        float currentY = spacing;
+        for (int i = 0; i < buttonTitles.Count; i++) {
+            string title = buttonTitles[i];
+            float x = (safeArea.width - buttonWidth) / 2;
+            if (GUI.Button(new Rect(x, currentY, buttonWidth, buttonHeight), title, buttonStyle)) {
+                HandleButtonClick(title);
+            }
+            currentY += buttonHeight + spacing;
+        }
+
+        GUI.EndScrollView();
+    }
+
+    private void HandleButtonClick(string title)
+    {
+        switch (title)
         {
-            PointPubSDK.Instance.EnableLogTrace();
-            PointPubSDK.Instance.StartOfferwall("userId");
+            case "EnableLogTrace":
+                PointPubSDK.Instance.EnableLogTrace();
+                break;
+
+            case "StartOfferwall":
+                PointPubSDK.Instance.SetAppId("<ENTER_YOUR_APP_ID>");
+                PointPubSDK.Instance.SetUserId("<ENTER_YOUR_USER_ID>");
+
+                PointPubSDK.Instance.StartOfferwall(() => {
+                    Debug.Log("Offerwall View Opened.");
+                }, () => {
+                    Debug.Log("Offerwall View Closed");
+                });
+                break;
+
+            default:
+                Debug.Log($"Unhandled button: {title}");
+                break;
         }
     }
 }
+
